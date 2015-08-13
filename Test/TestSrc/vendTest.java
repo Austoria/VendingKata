95c85eeea8d5c8a9e9be76ead91e038ea00bb70c
Austoria <teets.32@buckeyemail.osu.edu>
2015-08-13 17:53:05 -0400

    Create vending machine before tests refactor

--- Test/TestSrc/vendTest.java
+++ Test/TestSrc/vendTest.java
@@ -5,17 +5,16 @@ import org.junit.Test;
 import vendingSrc.VendingMachine;
 
 public class vendTest {
+	VendingMachine vender = new VendingMachine();
 	
 	@Test
 	public void whenVendingMachineIsCheckedItDoesSomething(){
-		VendingMachine vender = new VendingMachine();
 		assertEquals("INSERT COIN", vender.check());
 	}
 	
 	@Test
 	public void vendingMachineAcceptsCoinsAndDisplaysValue(){
 		//Machine accepts coin based on weight in milligrams
-		VendingMachine vender = new VendingMachine();
 		vender.insertCoin(5670);
 		assertEquals("25", vender.check());
 	}
