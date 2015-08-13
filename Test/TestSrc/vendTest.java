14650ea90ec379f89d584109d2d6d04ee301a40b
Austoria <teets.32@buckeyemail.osu.edu>
2015-08-13 17:51:52 -0400

    Vending Machine creates coin map and accepts coins

--- Test/TestSrc/vendTest.java
+++ Test/TestSrc/vendTest.java
@@ -1,14 +1,22 @@
 package TestSrc;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 import vendingSrc.VendingMachine;
 
 public class vendTest {
+	
 	@Test
 	public void whenVendingMachineIsCheckedItDoesSomething(){
-		assertEquals("INSERT COIN", VendingMachine.check());
+		VendingMachine vender = new VendingMachine();
+		assertEquals("INSERT COIN", vender.check());
+	}
+	
+	@Test
+	public void vendingMachineAcceptsCoinsAndDisplaysValue(){
+		//Machine accepts coin based on weight in milligrams
+		VendingMachine vender = new VendingMachine();
+		vender.insertCoin(5670);
+		assertEquals("25", vender.check());
 	}
 }
--- src/vendingSrc/VendingMachine.java
+++ src/vendingSrc/VendingMachine.java
@@ -1,9 +1,28 @@
 package vendingSrc;
 
-public class VendingMachine {
+import java.util.*;
 
-	public static String check() {
-		return "INSERT COIN";
+public class VendingMachine {
+	int value;
+	
+	public String check() {
+		String display = "INSERT COIN";
+		if (value > 0)
+			return String.valueOf(value);
+		return display;
 	}
 
+	public void insertCoin(int weight) {
+		HashMap<Integer, Integer> coins = new HashMap <Integer, Integer>();
+		HashMap<Integer, Integer> values = new HashMap <Integer, Integer>();
+		values.put(5670, 25);
+		values.put(5000, 5);
+		values.put(2268, 10);
+		
+		if (!coins.containsKey(weight))
+			coins.put(weight, 1);
+		else
+			coins.put(weight, coins.get(weight)+1);
+		value += values.get(weight);
+	}
 }
